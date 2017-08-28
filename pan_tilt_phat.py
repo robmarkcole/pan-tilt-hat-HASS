6a2eef0705ff6255dc15574c7341ebe4be547e76
robmarkcole <robmarkcole@gmail.com>
2017-08-28 06:01:43 +0100

    Add config

--- pan_tilt_phat.py
+++ pan_tilt_phat.py
@@ -1,8 +1,8 @@
 """
-Support for pan-tilt pHAT sensors.
+Support for pan-tilt pHAT.
 
 For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/sensor.pantiltphat
+https://home-assistant.io/components/sensor.pan_tilt_phat
 """
 
 import logging
@@ -11,25 +11,32 @@ from homeassistant.helpers.entity import Entity
 
 REQUIREMENTS = ['pantilthat==0.0.4']
 _LOGGER = logging.getLogger(__name__)
-DEFAULT_NAME = 'pantiltphat'
+DOMAIN = 'pan_tilt_phat'
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     try:
-        # pylint: disable=import-error
         import pantilthat
     except OSError:
         _LOGGER.error("No pan-tilt pHAT was found.")
         return False
 
-    add_devices([PanTiltStage()], True)
+    add_devices([PanTiltPhat()], True)
 
-class PanTiltStage(Entity):
+class PanTiltPhat(Entity):
     """Representation of the stage."""
+
+    ICON = 'mdi:camera-switch'
 
     def __init__(self):
         """Initialize the stage."""
-        self._name = DEFAULT_NAME
+        self._name = DOMAIN
         self._state = None       # json obj with pan and tilt
+
+    def home(self):
+        """Return the name of the sensor."""
+        self._pantilt.pan(0)
+        self._pantilt.tilt(0)
+        return
 
     @property
     def name(self):
@@ -44,4 +51,4 @@ class PanTiltStage(Entity):
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
-        return 'mdi:mdi-camera-switch'
+        return self.ICON
