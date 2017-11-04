3589ab985f9fd2965eea4b28ab9c92dd103951e8
robmarkcole <robmarkcole@gmail.com>
2017-11-04 15:20:21 +0000

    Updates to input_number
    Was input_slider, now input_number

--- python_scripts/input_number_control.py
+++ python_scripts/input_number_control.py
@@ -3,5 +3,5 @@ state = hass.states.get(entity_id).state   # Pan: angle
 pan_angle = state.split(':')[1]           # Gets the angle
 logger.warning("pan_angle {}".format(pan_angle))
 
-hass.services.call('input_slider', 'select_value',
-    {"entity_id":"input_slider.pan_control", "value": str(pan_angle)})
+hass.services.call('input_number', 'select_value',
+    {"entity_id":"input_number.pan_control", "value": str(pan_angle)})
